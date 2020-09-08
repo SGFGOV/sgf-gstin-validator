cd57627dcb0a7b06a40908160fb9fb500485f6c3
Srikanth L <srik.lak@gmail.com>
2020-09-08 11:34:58 +0000

    Remove assert

--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,28 @@
 var expect = require('chai').expect;
 var validator = require('./index');
-var assert = require('assert');
 
-describe('gstin-validator', function () {
-  it('isValidGSTNumber::Should return false when length is not 15 digits', function () {
+describe('gstin-validator', function() {
+  it('isValidGSTNumber::Should return false when length is not 15 digits', function() {
     var result = validator.isValidGSTNumber('123412341234');
     expect(result).to.be.false;
   });
 
-  it('isValidGSTNumber::Should return false if 15 digit non regex matching GSTIN is validated', function () {
+  it('isValidGSTNumber::Should return false if 15 digit non regex matching GSTIN is validated', function() {
     var result = validator.isValidGSTNumber('47AAGCG4576J1A6');
     expect(result).to.be.false;
   });
 
-  it('isValidGSTNumber::Should return false if 15 digit valid regex matching GSTIN with incorrect checksum is validated', function () {
+  it('isValidGSTNumber::Should return false if 15 digit valid regex matching GSTIN with incorrect checksum is validated', function() {
     var result = validator.isValidGSTNumber('27AAGCG4576J1Z8');
     expect(result).to.be.false;
   });
 
-  it('isValidGSTNumber::Should return true if valid 15 digit GSTIN is passed', function () {
+  it('isValidGSTNumber::Should return true if valid 15 digit GSTIN is passed', function() {
     var result = validator.isValidGSTNumber('27AAGCG4576J1Z6'); // Google India
     expect(result).to.be.true;
   });
 
-  it('isValidGSTNumber::Should return true for GSTIN of Indian Oil across all states', function () {
+  it('isValidGSTNumber::Should return true for GSTIN of Indian Oil across all states', function() {
     var gstins = ['37AAACI1681G2ZN', '38AAALS7119L1ZH', '35AAACI1681G1ZS',
       '12AAACI1681G1Z0', '18AAACI1681G1ZO', '10AAACI1681G1Z4',
       '04AAACI1681G1ZX', '22AAACI1681G1ZZ', '26AAACI1681G1ZR',
@@ -45,37 +44,37 @@ describe('gstin-validator', function () {
     expect(success).to.be.true;
   });
 
-  it('ValidateGSTIN::Should return Enter a valid 15 character GSTIN when length is not 15 digits', function () {
+  it('ValidateGSTIN::Should return Enter a valid 15 character GSTIN when length is not 15 digits', function() {
     var result = validator.ValidateGSTIN('123412341234');
     expect((result === 'Enter a valid 15 character GSTIN')).to.be.true;
   });
 
-  it('ValidateGSTIN::Should return Invalid GSTIN format if 15 digit non regex matching GSTIN is validated', function () {
+  it('ValidateGSTIN::Should return Invalid GSTIN format if 15 digit non regex matching GSTIN is validated', function() {
     var result = validator.ValidateGSTIN('47AAGCG4576J1A6');
     expect((result === 'Invalid GSTIN format')).to.be.true;
   });
 
-  it('ValidateGSTIN::Should return Invalid checksum character in GSTIN if 15 digit valid regex matching GSTIN with incorrect checksum is validated', function () {
+  it('ValidateGSTIN::Should return Invalid checksum character in GSTIN if 15 digit valid regex matching GSTIN with incorrect checksum is validated', function() {
     var result = validator.ValidateGSTIN('25AAACI1681G1Z4');
     expect((result === 'Invalid checksum character in GSTIN')).to.be.true;
   });
 
-  it('ValidateGSTIN::Should return true if valid 15 digit GSTIN is passed', function () {
+  it('ValidateGSTIN::Should return true if valid 15 digit GSTIN is passed', function() {
     var result = validator.ValidateGSTIN('27AAGCG4576J1Z6'); // Google India
     expect((result === 'Valid GSTIN')).to.be.true;
   });
 
-  it('getGSTINInfo::Should return verbose text for a valid GSTIN', function () {
+  it('getGSTINInfo::Should return verbose text for a valid GSTIN', function() {
     var result = validator.getGSTINInfo('27AAGCG4576J1Z6');
     expect((result === 'The GSTIN 27AAGCG4576J1Z6 is entity #1 belonging to Company whose PAN is AAGCG4576J registered in Maharashtra (MH)')).to.be.true;
   });
 
-  it('getGSTINInfo::Should return verbose text for a valid GSTIN of 11th entity of same PAN', function () {
+  it('getGSTINInfo::Should return verbose text for a valid GSTIN of 11th entity of same PAN', function() {
     var result = validator.getGSTINInfo('27AAGCG4576JBZW');
     expect((result === 'The GSTIN 27AAGCG4576JBZW is entity #11 belonging to Company whose PAN is AAGCG4576J registered in Maharashtra (MH)')).to.be.true;
   });
 
-  it('getGSTINInfo::Should return Invalid GSTIN on incorrect GSTIN', function () {
+  it('getGSTINInfo::Should return Invalid GSTIN on incorrect GSTIN', function() {
     var result = validator.getGSTINInfo('47AAGCG4576J1Z6');
     expect((result === 'Invalid GSTIN')).to.be.true;
   });
